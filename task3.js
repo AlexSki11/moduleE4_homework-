e15ae31a33a1c9337dfe974b28bbd314d693bdf7
AlexSki11 <sasha.morozov022@yandex.ru>
2025-01-05 02:44:36 +0300

    finish task3

--- task3.js
+++ task3.js
@@ -1,1 +1,8 @@
 console.log('Задание 3')
+
+function create_obj_without_proto (obj) {
+	return Object.create(null)
+}
+
+const obj_without_proto = create_obj_without_proto()
+console.log(obj_without_proto)
